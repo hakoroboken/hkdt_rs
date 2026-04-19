43d2f4b2f7e0a7e6085767b1b70d7a4d716c441f
motii8128 <default270robo@gmail.com>
2026-04-19 10:17:29 +0900

    [update] arm_bot_exampleへのコメントの追加

--- examples/arm_bot_example.rs
+++ examples/arm_bot_example.rs
@@ -1,28 +1,36 @@
+// このExampleはArmBotのセンサーデータをシリアル通信を介して取得します。
+
+// シリアル通信用ライブラリ
 use hkdt_rs::connection::serial::Serial;
+// ArmBotを処理するためのライブラリ
 use hkdt_rs::arm_bot::ArmBot;
-use hkdt_rs::log_info;
+// デバッグ出力用のマクロ
+use hkdt_rs::{log_info, log_err};
 
 fn main()
 {
+    // シリアルポートを開く。デバイス名とボーレートは環境に合わせて変更してください。
     let mut serial = Serial::new("/dev/ttyACM0", 115200).unwrap();
 
+    // ArmBotのインスタンスを作成
     let mut arm_bot = ArmBot::new();
     
+    // シリアル通信による受信、そしてセンサーデータの更新をループで行います。
     loop {
-        // let data = serial.read(&mut buf).unwrap_or_default();
-
-        // // let angle_data = (buf[1] as i16) << 8 | (buf[2] as i16);
-        // println!("Size:{}", data);
-        // println!("Received: [{}]", buf[0]);
-
-        let s = serial.read_str();
-
-        match s {
+        // シリアルポートからデータを読み取ります。
+        let from_arm_bot = serial.read_str();
+        
+        // 読み取ったデータはOption型で返されるため、Some(読み取り成功)かNone(読み取り失敗)かを確認します。
+        match from_arm_bot {
             Some(line) =>{
+                // 読み取ったデータをArmBotのセンサーデータとして更新します。
                 arm_bot.update_sensor(line);
+                // 更新されたセンサーデータをログに出力します。
                 log_info!("Hand : {:?}", arm_bot.get_hand_motor());
             },
-            None => println!("Failed to read from serial port"),
+            None => {
+                log_err!("データが空でした。シリアル通信に問題がある可能性があります。");
+            },
         }
     }
 }
