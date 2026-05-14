13a1410437e77334e39bbeb438ae33fced022af7
motii8128 <default270robo@gmail.com>
2026-05-14 18:24:38 +0900

    [new] brute forceアルゴリズムによる最近傍探索を追加

---
+++ src/algorithm/brute_force.rs
@@ -0,0 +1,26 @@
+use crate::common::Position2D;
+
+pub fn brute_force(
+    point_cloud : &[Position2D],
+    target : Position2D
+)->(usize, f32)
+{
+    let mut best_d = std::f32::INFINITY;
+    let mut best_i = 0;
+
+    let tx = target.x;
+    let ty = target.y;
+
+    for (i, p) in point_cloud.iter().enumerate() {
+        let dx = p.x - tx;
+        let dy = p.y - ty;
+        let d = dx * dx + dy * dy;
+
+        if d < best_d {
+            best_d = d;
+            best_i = i;
+        }
+    }
+
+    (best_i, best_d)
+}
--- src/algorithm/mod.rs
+++ src/algorithm/mod.rs
@@ -1,1 +1,3 @@
 pub mod kd_tree;
+pub mod imu_ekf;
+pub mod brute_force;
