cd7d2f1965e0f810ccc8dee819d2477c078164dd
motii8128 <default270robo@gmail.com>
2026-05-16 08:22:25 +0900

    [new] icpアルゴリズムの作成

--- src/localization/p2p_icp.rs
+++ src/localization/p2p_icp.rs
@@ -1,3 +1,5 @@
+use nalgebra::{Isometry2, Translation2};
+
 use crate::common::{Vec2, Position2D};
 use crate::algorithm::kd_tree::KdTree;
 use crate::log_err;
@@ -10,19 +12,19 @@ pub fn icp(
 {
     let kdtree = KdTree::new(target.clone());
 
-    let mut transformed = source.clone();
-
-    let mut transform = Position2D::new(0.0, 0.0, 0.0);
+    let mut transform = Isometry2::identity();
 
     for _ in 0..max_iterations
     {
         let mut pairs = Vec::new();
-        for (_, src_p) in transformed.iter().enumerate()
+        for (_, src_p) in source.iter().enumerate()
         {
-            match kdtree.nearest(&src_p)
+            let transformed_src = transform.transform_point(&nalgebra::Point2::new(src_p.x, src_p.y));
+            let transformed_src_p = Vec2::new(transformed_src.x, transformed_src.y);
+            match kdtree.nearest(&transformed_src_p)
             {
                 Some((nearest, _dist))=>{
-                    pairs.push((*src_p, nearest));
+                    pairs.push((transformed_src_p, nearest));
                 }
                 None=>{
 
@@ -37,8 +39,54 @@ pub fn icp(
             return Position2D::new(0.0, 0.0, 0.0)
         }
 
-        // let mut 
+        let mut source_centroid = Vec2::zeros();
+        let mut target_centroid = Vec2::zeros();
+
+        for (_, (src, tar)) in pairs.iter().enumerate()
+        {
+            source_centroid += src;
+            target_centroid += tar;
+        }
+
+        source_centroid /= pair_num as f32;
+        target_centroid /= pair_num as f32;
+
+        let mut s = 0.0;
+        let mut c = 0.0;
+
+        for (_, (src, tar)) in pairs.iter().enumerate() {
+
+            let centered_src = src - source_centroid;
+            let centered_tar = tar - target_centroid;
+
+            s += centered_src.x * centered_tar.y - centered_src.y * centered_tar.x;
+
+            c += centered_src.x * centered_tar.x + centered_src.y * centered_tar.y;
+        }
+
+        let yaw = s.atan2(c);
+
+        let rot = nalgebra::Rotation2::new(yaw);
+
+        let t = target_centroid - rot.matrix() * source_centroid;
+
+        let delta_trans = Isometry2::from_parts(Translation2::from(t), rot.into());
+
+        transform = delta_trans * transform;
+
+        let dx = delta_trans.translation.x;
+        let dy = delta_trans.translation.y;
+        let dtheta = delta_trans.rotation.angle();
+
+        if dx.abs() < 1e-6 && dy.abs() < 1e-6  && dtheta.abs() < 1e-6
+        {
+            break
+        }
     }    
 
-    transform
+    Position2D::new(
+        transform.translation.x, 
+        transform.translation.y, 
+        transform.rotation.angle()
+    )
 }
